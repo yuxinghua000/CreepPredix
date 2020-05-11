7ec8f0c6f39c513ef287fa9a39d19f9fe70f0fd6
yuxinghua000 <yu.345@osu.edu>
2020-05-11 14:21:44 -0400

    Add files via upload

--- visuals.py
+++ visuals.py
@@ -9,123 +9,154 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 ###########################################
 
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
 import numpy as np
-from sklearn.model_selection import learning_curve
-from sklearn.model_selection import validation_curve
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import ShuffleSplit, train_test_split
 
-def ModelLearning(X, y):
-    """ Calculates the performance of several models with varying sizes of training data.
-        The learning and testing scores for each model are then plotted. """
+def pca_results(good_data, pca):
+	'''
+	Create a DataFrame of the PCA results
+	Includes dimension feature weights and explained variance
+	Visualizes the PCA results
+	'''
 
-    # Create 10 cross-validation sets for training and testing
-    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 0)
+	# Dimension indexing
+	dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1,len(pca.components_)+1)]
 
-    # Generate the training set sizes increasing by 50
-    train_sizes = np.rint(np.linspace(1, X.shape[0]*0.8 - 1, 9)).astype(int)
+	# PCA components
+	components = pd.DataFrame(np.round(pca.components_, 4), columns = list(good_data.keys()))
+	components.index = dimensions
 
-    # Create the figure window
-    fig = pl.figure(figsize=(10,7))
+	# PCA explained variance
+	ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+	variance_ratios = pd.DataFrame(np.round(ratios, 4), columns = ['Explained Variance'])
+	variance_ratios.index = dimensions
 
-    # Create three different models based on max_depth
-    for k, depth in enumerate([1,3,6,10]):
+	# Create a bar plot visualization
+	fig, ax = plt.subplots(figsize = (14,8))
 
-        # Create a Decision tree regressor at max_depth = depth
-        regressor = DecisionTreeRegressor(max_depth = depth)
-
-        # Calculate the training and testing scores
-        sizes, train_scores, test_scores = learning_curve(regressor, X, y, \
-            cv = cv, train_sizes = train_sizes, scoring = 'r2')
-
-        # Find the mean and standard deviation for smoothing
-        train_std = np.std(train_scores, axis = 1)
-        train_mean = np.mean(train_scores, axis = 1)
-        test_std = np.std(test_scores, axis = 1)
-        test_mean = np.mean(test_scores, axis = 1)
-
-        # Subplot the learning curve
-        ax = fig.add_subplot(2, 2, k+1)
-        ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
-        ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
-        ax.fill_between(sizes, train_mean - train_std, \
-            train_mean + train_std, alpha = 0.15, color = 'r')
-        ax.fill_between(sizes, test_mean - test_std, \
-            test_mean + test_std, alpha = 0.15, color = 'g')
-
-        # Labels
-        ax.set_title('max_depth = %s'%(depth))
-        ax.set_xlabel('Number of Training Points')
-        ax.set_ylabel('Score')
-        ax.set_xlim([0, X.shape[0]*0.8])
-        ax.set_ylim([-0.05, 1.05])
-
-    # Visual aesthetics
-    ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
-    fig.suptitle('Decision Tree Regressor Learning Performances', fontsize = 16, y = 1.03)
-    fig.tight_layout()
-    fig.show()
+	# Plot the feature weights as a function of the components
+	components.plot(ax = ax, kind = 'bar');
+	ax.set_ylabel("Feature Weights")
+	ax.set_xticklabels(dimensions, rotation=0)
 
 
-def ModelComplexity(X, y):
-    """ Calculates the performance of the model as model complexity increases.
-        The learning and testing errors rates are then plotted. """
+	# Display the explained variance ratios
+	for i, ev in enumerate(pca.explained_variance_ratio_):
+		ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
 
-    # Create 10 cross-validation sets for training and testing
-    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 0)
+	# Return a concatenated DataFrame
+	return pd.concat([variance_ratios, components], axis = 1)
 
-    # Vary the max_depth parameter from 1 to 10
-    max_depth = np.arange(1,11)
+def cluster_results(reduced_data, preds, centers, pca_samples):
+	'''
+	Visualizes the PCA-reduced cluster data in two dimensions
+	Adds cues for cluster centers and student-selected sample data
+	'''
 
-    # Calculate the training and testing scores
-    train_scores, test_scores = validation_curve(DecisionTreeRegressor(), X, y, \
-        param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
+	predictions = pd.DataFrame(preds, columns = ['Cluster'])
+	plot_data = pd.concat([predictions, reduced_data], axis = 1)
 
-    # Find the mean and standard deviation for smoothing
-    train_mean = np.mean(train_scores, axis=1)
-    train_std = np.std(train_scores, axis=1)
-    test_mean = np.mean(test_scores, axis=1)
-    test_std = np.std(test_scores, axis=1)
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
 
-    # Plot the validation curve
-    pl.figure(figsize=(7, 5))
-    pl.title('Decision Tree Regressor Complexity Performance')
-    pl.plot(max_depth, train_mean, 'o-', color = 'r', label = 'Training Score')
-    pl.plot(max_depth, test_mean, 'o-', color = 'g', label = 'Validation Score')
-    pl.fill_between(max_depth, train_mean - train_std, \
-        train_mean + train_std, alpha = 0.15, color = 'r')
-    pl.fill_between(max_depth, test_mean - test_std, \
-        test_mean + test_std, alpha = 0.15, color = 'g')
+	# Color map
+	cmap = cm.get_cmap('gist_rainbow')
 
-    # Visual aesthetics
-    pl.legend(loc = 'lower right')
-    pl.xlabel('Maximum Depth')
-    pl.ylabel('Score')
-    pl.ylim([-0.05,1.05])
-    pl.show()
+	# Color the points based on assigned cluster
+	for i, cluster in plot_data.groupby('Cluster'):   
+	    cluster.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+	                 color = cmap((i)*1.0/(len(centers)-1)), label = 'Cluster %i'%(i), s=30);
+
+	# Plot centers with indicators
+	for i, c in enumerate(centers):
+	    ax.scatter(x = c[0], y = c[1], color = 'white', edgecolors = 'black', \
+	               alpha = 1, linewidth = 2, marker = 'o', s=200);
+	    ax.scatter(x = c[0], y = c[1], marker='$%d$'%(i), alpha = 1, s=100);
+
+	# Plot transformed sample points 
+	ax.scatter(x = pca_samples[:,0], y = pca_samples[:,1], \
+	           s = 150, linewidth = 4, color = 'black', marker = 'x');
+
+	# Set plot title
+	ax.set_title("Cluster Learning on PCA-Reduced Data - Centroids Marked by Number\nTransformed Sample Data Marked by Black Cross");
 
 
-def PredictTrials(X, y, fitter, data):
-    """ Performs trials of fitting and predicting data. """
+def biplot(good_data, reduced_data, pca):
+    '''
+    Produce a biplot that shows a scatterplot of the reduced
+    data and the projections of the original features.
+    
+    good_data: original data, before transformation.
+               Needs to be a pandas dataframe with valid column names
+    reduced_data: the reduced data (the first two dimensions are plotted)
+    pca: pca object that contains the components_ attribute
+    return: a matplotlib AxesSubplot object (for any additional customization)
+    
+    This procedure is inspired by the script:
+    https://github.com/teddyroland/python-biplot
+    '''
 
-    # Store the predicted prices
-    prices = []
+    fig, ax = plt.subplots(figsize = (14,8))
+    # scatterplot of the reduced data    
+    ax.scatter(x=reduced_data.loc[:, 'Dimension 1'], y=reduced_data.loc[:, 'Dimension 2'], 
+        facecolors='b', edgecolors='b', s=70, alpha=0.5)
+    
+    feature_vectors = pca.components_.T
 
-    for k in range(10):
-        # Split the data
-        X_train, X_test, y_train, y_test = train_test_split(X, y, \
-            test_size = 0.2, random_state = k)
+    # we use scaling factors to make the arrows easier to see
+    arrow_size, text_pos = 7.0, 8.0,
 
-        # Fit the data
-        reg = fitter(X_train, y_train)
+    # projections of the original features
+    for i, v in enumerate(feature_vectors):
+        ax.arrow(0, 0, arrow_size*v[0], arrow_size*v[1], 
+                  head_width=0.2, head_length=0.2, linewidth=2, color='red')
+        ax.text(v[0]*text_pos, v[1]*text_pos, good_data.columns[i], color='black', 
+                 ha='center', va='center', fontsize=18)
 
-        # Make a prediction
-        pred = reg.predict([data[0]])[0]
-        prices.append(pred)
+    ax.set_xlabel("Dimension 1", fontsize=14)
+    ax.set_ylabel("Dimension 2", fontsize=14)
+    ax.set_title("PC plane with original feature projections.", fontsize=16);
+    return ax
+    
 
-        # Result
-        print("Trial {}: ${:,.2f}".format(k+1, pred))
+def channel_results(reduced_data, outliers, pca_samples):
+	'''
+	Visualizes the PCA-reduced cluster data in two dimensions using the full dataset
+	Data is labeled by "Channel" and cues added for student-selected sample data
+	'''
 
-    # Display price range
-    print("\nRange in prices: ${:,.2f}".format(max(prices) - min(prices)))
+	# Check that the dataset is loadable
+	try:
+	    full_data = pd.read_csv("customers.csv")
+	except:
+	    print("Dataset could not be loaded. Is the file missing?")       
+	    return False
+
+	# Create the Channel DataFrame
+	channel = pd.DataFrame(full_data['Channel'], columns = ['Channel'])
+	channel = channel.drop(channel.index[outliers]).reset_index(drop = True)
+	labeled = pd.concat([reduced_data, channel], axis = 1)
+	
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Color map
+	cmap = cm.get_cmap('gist_rainbow')
+
+	# Color the points based on assigned Channel
+	labels = ['Hotel/Restaurant/Cafe', 'Retailer']
+	grouped = labeled.groupby('Channel')
+	for i, channel in grouped:   
+	    channel.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+	                 color = cmap((i-1)*1.0/2), label = labels[i-1], s=30);
+	    
+	# Plot transformed sample points   
+	for i, sample in enumerate(pca_samples):
+		ax.scatter(x = sample[0], y = sample[1], \
+	           s = 200, linewidth = 3, color = 'black', marker = 'o', facecolors = 'none');
+		ax.scatter(x = sample[0]+0.25, y = sample[1]+0.3, marker='$%d$'%(i), alpha = 1, s=125);
+
+	# Set plot title
+	ax.set_title("PCA-Reduced Data Labeled by 'Channel'\nTransformed Sample Data Circled");
